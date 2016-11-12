c573e60a8522f21ef382a7b8d894bf859936f375
Divay Prakash <divayprakash3@gmail.com>
2016-11-13 00:57:56 +0530

    Update

--- app/src/main/java/io/github/divayprakash/asynctask/MainActivity.java
+++ app/src/main/java/io/github/divayprakash/asynctask/MainActivity.java
@@ -1,20 +1,37 @@
 package io.github.divayprakash.asynctask;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.main_text);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,6 +45,14 @@ public class MainActivity extends AppCompatActivity {
                             public void onClick(View v) {}
                         })
                         .show();
+                String url = "https://iiitd.ac.in/about";
+                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    new AsyncTaskRunner().execute(url);
+                } else {
+                    textView.setText("No network connection available.");
+                }
             }
         });
     }
@@ -45,7 +70,49 @@ public class MainActivity extends AppCompatActivity {
             this.finish();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, Void, String> {
+        private static final String DEBUG_TAG = "AsyncTaskDemo";
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page!";
+            }
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+        }
+        private String downloadUrl(String myurl) throws IOException, UnsupportedEncodingException {
+            InputStream inputStream = null;
+            int length = 500;
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000);
+                conn.setConnectTimeout(15000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d(DEBUG_TAG, "The response is: " + response);
+                inputStream = conn.getInputStream();
+                Reader reader = new InputStreamReader(inputStream, "UTF-8");
+                char[] buffer = new char[length];
+                reader.read(buffer);
+                reader.close();
+                String contentAsString = new String(buffer);
+                return contentAsString;
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            }
+        }
     }
 }
