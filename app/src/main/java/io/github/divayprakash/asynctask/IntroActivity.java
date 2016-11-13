05c8c14f2d6e32d94bc47872b87b39102d47d236
Divay Prakash <divayprakash3@gmail.com>
2016-11-13 23:04:50 +0530

    Add IntroActivity without associated layout XML

---
+++ app/src/main/java/io/github/divayprakash/asynctask/IntroActivity.java
@@ -0,0 +1,12 @@
+package io.github.divayprakash.asynctask;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IntroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
