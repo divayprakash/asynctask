64cad71535590f3e8986aa868ec83b0e762b27f5
Divay Prakash <divayprakash3@gmail.com>
2016-11-14 00:08:14 +0530

    Update IntroActivity

--- app/src/main/java/io/github/divayprakash/asynctask/IntroActivity.java
+++ app/src/main/java/io/github/divayprakash/asynctask/IntroActivity.java
@@ -1,12 +1,35 @@
 package io.github.divayprakash.asynctask;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class IntroActivity extends Activity {
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
 
+public class IntroActivity extends AppIntro {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String slideTitle = getString(R.string.slide1title);
+        String slideDescription = getString(R.string.slide1description);
+        int slideBackground = getResources().getColor(R.color.slide1background);
+        addSlide(AppIntroFragment.newInstance(slideTitle, slideDescription, R.drawable.slide1image, slideBackground));
+        slideTitle = getString(R.string.slide2title);
+        slideDescription = getString(R.string.slide2description);
+        slideBackground = getResources().getColor(R.color.slide2background);
+        addSlide(AppIntroFragment.newInstance(slideTitle, slideDescription, R.drawable.slide2image, slideBackground));
+        slideTitle = getString(R.string.slide3title);
+        slideDescription = getString(R.string.slide3description);
+        slideBackground = getResources().getColor(R.color.slide3background);
+        addSlide(AppIntroFragment.newInstance(slideTitle, slideDescription, R.drawable.slide3image, slideBackground));
+        showSkipButton(false);
+        setVibrate(true);
+        setVibrateIntensity(30);
+        setDepthAnimation();
+    }
+    @Override
+    public void onDonePressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
